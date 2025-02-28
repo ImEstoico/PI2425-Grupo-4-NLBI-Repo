d7fa99465ac622728c51c1aa6117b8b2cce39e2c
luarsa04 <lucargsan@alu.edu.gva.es>
2025-02-28 13:28:48 +0100

    Clases ya cambiadas

---
+++ Cliente.java
@@ -0,0 +1,64 @@
+package tienda.Model;
+
+class Cliente extends Usuario {
+    private int numeroPedidos;
+    private boolean tieneTarjetaFidelizacion;
+    private String direccionEnvio;
+    private Metodo_Pago metodoPago;
+
+
+    public Cliente(String dni, String nombre, String apellidos, int telefono, String direccion, String email, int numeroPedidos,
+                   boolean tieneTarjetaFidelizacion, String direccionEnvio, Metodo_Pago metodoPago) {
+        super(dni, nombre, apellidos, telefono, direccion, email);
+        this.numeroPedidos = numeroPedidos;
+        this.tieneTarjetaFidelizacion = tieneTarjetaFidelizacion;
+        this.direccionEnvio = direccionEnvio;
+        this.metodoPago = metodoPago;
+    }
+
+
+    public int getNumeroPedidos() {
+        return numeroPedidos;
+    }
+
+    public boolean isTieneTarjetaFidelizacion() {
+        return tieneTarjetaFidelizacion;
+    }
+
+    public String getDireccionEnvio() {
+        return direccionEnvio;
+    }
+
+    public Metodo_Pago getMetodoPago() {
+        return metodoPago;
+    }
+
+    public void setNumeroPedidos(int numeroPedidos) {
+        this.numeroPedidos = numeroPedidos;
+    }
+
+    public void setTieneTarjetaFidelizacion(boolean tieneTarjetaFidelizacion) {
+        this.tieneTarjetaFidelizacion = tieneTarjetaFidelizacion;
+    }
+
+    public void setDireccionEnvio(String direccionEnvio) {
+        this.direccionEnvio = direccionEnvio;
+    }
+
+    public void setMetodoPago(Metodo_Pago metodoPago) {
+        this.metodoPago = metodoPago;
+    }
+
+    public void realizarPedido() {
+        if (numeroPedidos > 0) {
+            System.out.println("Realizando pedido...");
+        } else {
+            System.out.println("Debe registrarse como cliente primero.");
+        }
+    }
+
+    public void verArticulos() {
+        System.out.println("Visualizando artículos de la tienda...");
+    }
+}
+
