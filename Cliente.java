dccd9e87ddac98ad36f07a3e5c5813914987d83e
luarsa04 <lucargsan@alu.edu.gva.es>
2025-03-12 13:36:17 +0100

    Correcciones v2

---
+++ Cliente.java
@@ -0,0 +1,63 @@
+class Cliente extends Usuario {
+
+    private int numeroPedidos;
+    private boolean tieneTarjetaFidelizacion;
+    private String direccionEnvio;
+    private Metodo_Pago metodoPago;
+
+
+    public Cliente(String dni, String nombre, String apellidos, int telefono, String direccion, String email, boolean activo, String pass, int numeroPedidos,
+                   boolean tieneTarjetaFidelizacion, String direccionEnvio, Metodo_Pago metodoPago) {
+        super(dni, nombre, apellidos, telefono, direccion, email, activo, pass);
+        this.numeroPedidos = numeroPedidos;
+        this.tieneTarjetaFidelizacion = tieneTarjetaFidelizacion;
+        this.direccionEnvio = direccionEnvio;
+        this.metodoPago = metodoPago;
+    }
+
+
+    public int getNumeroPedidos() {
+        return numeroPedidos;
+    }
+
+    public boolean isTieneTarjetaFidelizacion() {
+        return tieneTarjetaFidelizacion;
+    }
+
+    public String getDireccionEnvio() {
+        return direccionEnvio;
+    }
+
+    public Metodo_Pago getMetodoPago() {
+        return metodoPago;
+    }
+
+    public void setNumeroPedidos(int numeroPedidos) {
+        this.numeroPedidos = numeroPedidos;
+    }
+
+    public void setTieneTarjetaFidelizacion(boolean tieneTarjetaFidelizacion) {
+        this.tieneTarjetaFidelizacion = tieneTarjetaFidelizacion;
+    }
+
+    public void setDireccionEnvio(String direccionEnvio) {
+        this.direccionEnvio = direccionEnvio;
+    }
+
+    public void setMetodoPago(Metodo_Pago metodoPago) {
+        this.metodoPago = metodoPago;
+    }
+
+    public void realizarPedido() {
+        if (numeroPedidos > 0) {
+            System.out.println("Realizando pedido...");
+        } else {
+            System.out.println("Debe registrarse como cliente primero.");
+        }
+    }
+
+    public void verArticulos() {
+        System.out.println("Visualizando artículos de la tienda...");
+    }
+}
+
---
+++ Pedido.java
@@ -0,0 +1,81 @@
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Pedido {
+
+    private int numeroPedido ;
+    private Date fecha ;
+    private String dir_envio;
+    private enum EstadoPedido{}
+    ArrayList<LineaPedido> lineaPedidos = new ArrayList<LineaPedido>();
+    ArrayList<Cliente> clientes = new ArrayList<Cliente>();
+
+    //Contructor
+    public Pedido(int numeroPedido, Date fecha, String dir_envio, ArrayList<LineaPedido> lineaPedidos, ArrayList<Cliente> clientes) {
+        this.numeroPedido = numeroPedido;
+        this.fecha = fecha;
+        this.dir_envio = dir_envio;
+        this.lineaPedidos = lineaPedidos;
+        this.clientes = clientes;
+    }
+
+    //Getter
+    public int getNumeroPedido() {
+        return numeroPedido;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public String getDir_envio() {
+        return dir_envio;
+    }
+
+    public ArrayList<LineaPedido> getLineaPedidos() {
+        return lineaPedidos;
+    }
+
+    public ArrayList<Cliente> getClientes() {
+        return clientes;
+    }
+
+
+    //setter
+    public void setNumeroPedido(int numeroPedido) {
+        this.numeroPedido = numeroPedido;
+    }
+
+    public void setLineaPedidos(ArrayList<LineaPedido> lineaPedidos) {
+        this.lineaPedidos = lineaPedidos;
+    }
+
+    public void setClientes(ArrayList<Cliente> clientes) {
+        this.clientes = clientes;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public void setDir_envio(String dir_envio) {
+        this.dir_envio = dir_envio;
+    }
+
+    public void mostrarPedido() {
+        System.out.println("mostrar pedido");
+    }
+
+    public void calcularTotal() {
+        System.out.println("calcular total de pedidos");
+    }
+
+    @Override
+    public String toString() {
+        return "Pedido{" +
+                "numeroPedido=" + numeroPedido +
+                ", fechaPedido=" + fecha +
+                ", direccEntrega='" + dir_envio + '\'' +
+                '}';
+    }
+}
